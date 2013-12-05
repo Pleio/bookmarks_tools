d497a421a9f5229212a8e43cf74c92167a9a77dc
Jeroen Dalsem <jdalsem@coldtrick.com>
2013-12-05 11:13:19 +0100

    added dutch translation

--- languages/en.php
+++ languages/en.php
@@ -1,12 +1,12 @@
 <?php
 
-	$english = array(
+$english = array(
 		'bookmarks_tools:settings:link_behaviour:title' => "Link behaviour",
 		'bookmarks_tools:settings:link_behaviour:description' => "The bookmarks plugin shows a link to a bookmarked resource. Here you can change the behaviour.",
 		'bookmarks_tools:settings:link_behaviour:options:default' => "Default behaviour (link opens in same window)",
 		'bookmarks_tools:settings:link_behaviour:options:blank_external' => "External links open in a new window",
 		'bookmarks_tools:settings:link_behaviour:options:blank_icon' => "An extra icon next to the link should open the link in a new window. Clicking the link keeps default behaviour",
 		'bookmarks_tools:entity:href:new_window' => "Open in a new window",
-	);
-	
-	add_translation("en", $english);
+);
+
+add_translation("en", $english);
---
+++ languages/nl.php
@@ -0,0 +1,10 @@
+<?php
+$language = array(
+		'bookmarks_tools:settings:link_behaviour:title' => 'Link gedrag',
+		'bookmarks_tools:settings:link_behaviour:description' => 'De bookmarks plugin toont een link naar een opslagen url. Hier kun je het gedrag van die link aanpassen.',
+		'bookmarks_tools:settings:link_behaviour:options:default' => 'Standaard gedrag (link opent in hetzelfde scherm)',
+		'bookmarks_tools:settings:link_behaviour:options:blank_external' => 'Externe links openen in een nieuw scherm',
+		'bookmarks_tools:settings:link_behaviour:options:blank_icon' => 'Een extra icoon naast de link opent de url in een nieuw scherm. De link zelf behoudt het standaard gedrag.',
+		'bookmarks_tools:entity:href:new_window' => 'Open in een nieuw scherm',
+);
+add_translation("nl", $language);
